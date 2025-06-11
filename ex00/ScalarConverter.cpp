2cfc82e395950b41a45b9149cc66a951056f6207
acta0724 <iwasakatsuya@icloud.com>
2025-06-12 00:00:29 +0900

    [fix] isDoubleLiteral & isFloatLiteral

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -7,19 +7,21 @@
 #include <string>
 #include <stdexcept>
 
-ScalarConverter::ScalarConverter() {}
-
-ScalarConverter::~ScalarConverter() {}
-
-bool ScalarConverter::isPseudoLiteral(const std::string &str) {
-    return str == "nan" || str == "nanf" || str == "+inf" || str == "+inff" || str == "-inf" || str == "-inff";
+bool isPseudoLiteral(const std::string &str) {
+    return str == "nan" || str == "nanf" || str == "+inf" || \
+        str == "+inff" || str == "-inf" || str == "-inff";
 }
 
-bool ScalarConverter::isCharLiteral(const std::string &str) {
-    return (str.size() == 3 && str[0] == '\'' && str[2] == '\'' && std::isprint(str[1]));
+bool isCharLiteral(const std::string &str) {
+    if (str.size() == 1 && std::isprint(str[0])) {
+        return true;
+    }
+    return false;
 }
 
-bool ScalarConverter::isIntLiteral(const std::string &str) {
+bool isIntLiteral(const std::string &str) {
+    if (str.empty())
+        return false;
     size_t i = 0;
     if (str[i] == '-' || str[i] == '+')
         ++i;
@@ -31,40 +33,82 @@ bool ScalarConverter::isIntLiteral(const std::string &str) {
     return true;
 }
 
-bool ScalarConverter::isFloatLiteral(const std::string &str) {
+bool isFloatLiteral(const std::string &str) {
+    if (str.empty())
+        return false;
     if (str.back() != 'f')
         return false;
     std::string tmp = str.substr(0, str.size() - 1);
-    return (tmp.find('.') != std::string::npos || isPseudoLiteral(str));
+    bool has_digit = false;
+    int dot_count = 0;
+    size_t i = 0;
+    if (tmp[0] == '-' || tmp[0] == '+')
+        ++i;
+    for (; i < tmp.size(); ++i) {
+        if (std::isdigit(tmp[i])) {
+            has_digit = true;
+        } else if (tmp[i] == '.') {
+            dot_count++;
+        } else {
+            return false;
+        }
+    }
+    if (dot_count > 1)
+        return false;
+    return has_digit;
 }
 
-bool ScalarConverter::isDoubleLiteral(const std::string &str) {
-    return (str.find('.') != std::string::npos && str.back() != 'f');
+bool isDoubleLiteral(const std::string &str) {
+    if (str.empty())
+        return false;
+    if (str.back() == 'f')
+        return false;
+    bool has_digit = false;
+    int dot_count = 0;
+    size_t i = 0;
+    if (str[0] == '-' || str[0] == '+')
+        ++i;
+    for (; i < str.size(); ++i) {
+        if (std::isdigit(str[i])) {
+            has_digit = true;
+        } else if (str[i] == '.') {
+            dot_count++;
+        } else {
+            return false;
+        }
+    }
+    if (dot_count > 1)
+        return false;
+    return has_digit;
 }
 
-void ScalarConverter::printChar(const double &value) {
+void printChar(const double &value) {
     std::cout << "char: ";
-    if (std::isnan(value) || std::isinf(value) || value < std::numeric_limits<char>::min() || value > std::numeric_limits<char>::max()) {
+    if (std::isnan(value) || std::isinf(value) || \
+        value < std::numeric_limits<char>::min() || \
+        value > std::numeric_limits<char>::max()) {
         std::cout << "impossible" << std::endl;
     } else {
         char c = static_cast<char>(value);
         if (std::isprint(c))
             std::cout << "'" << c << "'" << std::endl;
         else
-            std::cout << "non displayable" << std::endl;
+            std::cout << "Non displayable" << std::endl;
     }
 }
 
-void ScalarConverter::printInt(const double &value) {
+void printInt(const double &value) {
     std::cout << "int: ";
-    if (std::isnan(value) || std::isinf(value) || value < std::numeric_limits<int>::min() || value > std::numeric_limits<int>::max()) {
+    if (std::isnan(value) || std::isinf(value) || \
+        value < std::numeric_limits<int>::min() || \
+        value > std::numeric_limits<int>::max()) {
         std::cout << "impossible" << std::endl;
     } else {
         std::cout << static_cast<int>(value) << std::endl;
     }
 }
 
-void ScalarConverter::printFloat(const double &value) {
+void printFloat(const double &value) {
     std::cout << "float: ";
     if (std::isnan(value)) {
         std::cout << "nanf" << std::endl;
@@ -74,11 +118,12 @@ void ScalarConverter::printFloat(const double &value) {
         else
             std::cout << "-inff" << std::endl;
     } else {
-        std::cout << std::fixed << std::setprecision((fmod(value, 1) == 0 ? 1 : 1)) << static_cast<float>(value) << "f" << std::endl;
+        std::cout << std::fixed << std::setprecision(1) \
+            << static_cast<float>(value) << "f" << std::endl;
     }
 }
 
-void ScalarConverter::printDouble(const double &value) {
+void printDouble(const double &value) {
     std::cout << "double: ";
     if (std::isnan(value)) {
         std::cout << "nan" << std::endl;
@@ -88,7 +133,8 @@ void ScalarConverter::printDouble(const double &value) {
         else
             std::cout << "-inf" << std::endl;
     } else {
-        std::cout << std::fixed << std::setprecision((fmod(value, 1) == 0 ? 1 : 1)) << value << std::endl;
+        std::cout << std::fixed << std::setprecision(1) \
+            << value << std::endl;
     }
 }
 
@@ -96,16 +142,13 @@ void ScalarConverter::convert(const std::string &str) {
     double value;
     try {
         if (isPseudoLiteral(str)) {
-            if (str.back() == 'f')
-                value = std::stof(str);
-            else
-                value = std::stod(str);
+            value = std::stod(str);
         } else if (isCharLiteral(str)) {
-            value = static_cast<double>(str[1]);
+            value = static_cast<double>(str[0]);
         } else if (isIntLiteral(str)) {
-            value = std::stoi(str);
+            value = std::stod(str);
         } else if (isFloatLiteral(str)) {
-            value = std::stof(str);
+            value = std::stod(str.substr(0, str.size() - 1));
         } else if (isDoubleLiteral(str)) {
             value = std::stod(str);
         } else {
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,20 +6,10 @@
 
 class ScalarConverter {
  private:
-    ScalarConverter(const ScalarConverter &src);
-    ScalarConverter &operator=(const ScalarConverter &src);
-    static bool isPseudoLiteral(const std::string &str);
-    static bool isCharLiteral(const std::string &str);
-    static bool isIntLiteral(const std::string &str);
-    static bool isFloatLiteral(const std::string &str);
-    static bool isDoubleLiteral(const std::string &str);
-    static void printChar(const double &value);
-    static void printInt(const double &value);
-    static void printFloat(const double &value);
-    static void printDouble(const double &value);
+    ScalarConverter() = delete;
+    ScalarConverter(const ScalarConverter &src) = delete;
+    ScalarConverter &operator=(const ScalarConverter &src) = delete;
  public:
-    ScalarConverter();
-    ~ScalarConverter();
     static void convert(const std::string &str);
 };
 
