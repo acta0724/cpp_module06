7d44c9486de06ef92045253b5ed46865fc611edf
acta0724 <iwasakatsuya@icloud.com>
2025-06-12 00:40:44 +0900

    [fix] switch->if

--- ex02/generate.cpp
+++ ex02/generate.cpp
@@ -5,13 +5,13 @@
 #include "C.hpp"
 
 Base* generate() {
-    switch (std::rand() % 3) {
-        case 0:
-            return new A();
-        case 1:
-            return new B();
-        case 2:
-            return new C();
+    int random = std::rand() % 3;
+
+    if (random == 0) {
+        return new A();
+    } else if (random == 1) {
+        return new B();
+    } else {
+        return new C();
     }
-    return nullptr;
 }
